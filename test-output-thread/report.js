654954260f4a8e866a35179aa5fdfe1e3685fc46
saikiran <saikiran@gmail.com>
2023-11-29 12:14:53 +0530

    first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"3effb9e0-8499-430c-95bf-43fec7c2c526","feature":"Create account on wiki page","scenario":"User creates account on wiki page using input from cucumber data table","start":1701230973121,"group":1,"content":"","tags":"","end":1701230993462,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
